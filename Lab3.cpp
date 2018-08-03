4f3ec5ba11f4cf60a751de2eff87f92166f6a6ff
3ddyRomero <eduardromero@unitec.edu>
2018-08-03 15:24:26 -0600

    segundo

--- Lab3.cpp
+++ Lab3.cpp
@@ -9,6 +9,8 @@ using namespace std;
 int menu();
 int Eluno();
 int ElDos();
+int pascal();
+int fact();
 int ElTres();
 
 
@@ -58,16 +60,37 @@ int menu(){
 
 
 int Eluno(){
-int a,b,c;
+int a,b,c,p1=0,contax=0,p2;
+int result;
+int poli[2];
+
 	cout<<"Ingrese el valor de x^2: ";
 	cin>>a;
 	cout<<"Ingrese el valor de x: ";
 	cin>>b;
 	cout<<"ingrese el valor constante: ";
 	cin>>c;
+	
+	/*for(int i = 0;i<=poli.length();i++){
+		
+	}*/
 
+	poli[a];
+       	poli[b];
+	poli[c];
 
+	int bcuad = pow(b,2);
+	
+	int interior = bcuad-(4*a*c);
 
+	if(interior<0){
+		cout<<"****No es Factorizable****"<<endl;
+	}else{
+		result = -b + sqrt(interior);
+		cout<<"(x + "<<(result/2*a)*-1<<")"
+		    <<"(x + "<<(result/2*a)*-1<<")"<<endl;
+
+	}
 
 
 return 0;
@@ -75,10 +98,45 @@ return 0;
 
 int ElDos(){
 
+
+}
+
+int fact(int n){
+    if(n<2){
+        return 1;
+    }else{
+        return n * fact(n-1);
+    }
+}
+
+int pascal(int a, int b){
+
+	if(b == 1){
+	   return a;
+	}else{
+        if (a == b)
+            return 1;
+        else{
+            return fact(a)/ (fact(b) * fact(a - b));
+	}
+    }
+
 }
 
 
 int ElTres(){
-
-
+int SIZE; 
+int t=30;
+cout<<"Ingrese el tamaño de la secuaencia: "<<endl;
+cin>>SIZE;
+     for(int i=0; i<=SIZE; i++) {
+	     cout<<setw(t-i);
+        for(int c = 0; c<=i; c++)
+      	      
+	cout << pascal(i, c) << " ";
+        cout<<endl;
+    }
+    return 0;
 }
+
+
